782e84bfa89a6abbf88f47e50ecb82d4470e50bb
Blaser993 <corradodepinto.design@gmail.com>
2023-03-21 15:35:49 +0100

    FEAT basic layout, vedo lung. inserita per la psw

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+
+$lunghezza = $_GET["lunghezza"]
+
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,8 +21,22 @@
 </head>
 <body>
 
+<div class="container">
 
-
+    <label for="password" class="form-label">Generatore di password quasi sicure</label>
+    <form action="" method="GET">
+        <div class="container row">
+            <input type="text" name="lunghezza" id="password" class="col-10 flex-grow-1" placeholder="Inserisci la lunghezza in numeri della password richiesta">
+            <button class="col-auto  ms-2" type="submit">Genera</button>
+        </div>
+        <div>
+            Ecco la tua password: 
+            <?php
+            echo $lunghezza
+            ?>
+        </div>
+    </form>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
    
